c4e8f5e69808cf07a6e891c4ff47201bcd6b55ac
Carlos <A01708634@tec.mx>
2024-11-10 19:18:55 -0600

    Auth Layout and Auth Form

---
+++ lib/utils.ts
@@ -0,0 +1,18 @@
+import { FormType } from '@/types';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { z } from 'zod';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const authFormSchema = (formType: FormType) => {
+  return z.object({
+    fullName:
+      formType === 'sign-up'
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+    email: z.string().email(),
+  });
+};
